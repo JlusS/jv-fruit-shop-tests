c82cc0706da2937c83a1500ab5bf69311ff5094c
Egor <llikojia162@gmail.com>
2025-06-05 14:51:32 +0200

    fixed core.basesyntax.exception.InvalidDataException

--- src/test/java/core/basesyntax/file/impl/FileReaderImplTest.java
+++ src/test/java/core/basesyntax/file/impl/FileReaderImplTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class FileReaderImplTest {
-    private final String correctFile = "src/test/resources/data.csv";
+    private final String correctFile = "src/main/resources/data.csv";
     private FileReader fileReader;
 
     @BeforeEach
